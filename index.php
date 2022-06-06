d167fa958eb198c40e2b83f69425bd38062bc7e2
mligon115 <mligon115@yahoo.com>
2022-06-06 12:20:14 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php 
+    $db_host = "localhost";
+    $db_name = "cms";
+    $db_user = "kaygoss";
+    $db_pass = "WFOIhqv.A@hZdc..";
+
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    
+    if(mysqli_connect_error()){
+        echo mysqli_connect_error();
+        exit;
+    }
+    echo "Connected succesfully!"
+?>
