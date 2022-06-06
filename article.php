1498e5a547c0632b09112f6029b43e094cc6988f
mligon115 <mligon115@yahoo.com>
2022-06-06 14:54:56 -0500

    added article page to access one single post

--- article.php
+++ article.php
@@ -13,14 +13,14 @@
     
     $sql = "SELECT * 
             FROM article 
-            ORDER BY published_at";
+            WHERE id = " . $_GET['id'];
 
     $results = mysqli_query($conn, $sql);
 
     if ($results === false){
         echo mysqli_error($conn);
     }else{
-        $articles = mysqli_fetch_all($results, MYSQLI_ASSOC); 
+        $article = mysqli_fetch_assoc($results); 
     }
 ?>
 
@@ -38,19 +38,13 @@
     </header>
     <main>
         <!-- ADD A CHECK BEFORE THE LOOP TO CHECK FOR EMPTY ENTRIES -->
-        <?php if (empty($articles)): ?>
+        <?php if ($article === null): ?>
             <p>No articles found.</p>
         <?php else: ?>
-            <ul>
-                <?php foreach($articles as $article): ?>
-                    <li>
-                        <article>
-                            <h2><a href="article.php?id=<?= $article['id']; ?>"><?= $article['title']; ?></a></h2>
-                            <p><?= $article['content']; ?></p>
-                        </article>
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+            <article>
+                 <h2><?= $article['title']; ?></h2>
+                 <p><?= $article['content']; ?></p>
+            </article> 
         <?php endif; ?>
     </main>
 </body>
