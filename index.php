cd9f5572f2f0cea4e5366dc53baef625d45a4700
mligon115 <mligon115@yahoo.com>
2022-06-06 12:48:23 -0500

    connected database

--- index.php
+++ index.php
@@ -10,5 +10,21 @@
         echo mysqli_connect_error();
         exit;
     }
-    echo "Connected succesfully!"
+    
+    $sql = "SELECT * 
+            FROM article 
+            ORDER BY published_at";
+
+    $results = mysqli_query($conn, $sql);
+
+    if ($results === false){
+        echo mysqli_error($conn);
+    }else{
+        $articles = mysqli_fetch_all($results, MYSQLI_ASSOC); 
+         echo "<pre>";
+         print_r($articles);
+         echo "</pre>";
+    }
+
+ 
 ?>
